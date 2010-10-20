896faf4093742685ad4276cb76072947fbfa1f22
Dr. Drang <drdrang@gmail.com>
2010-10-19 20:55:25 -0500

    Flip HTML order of #content and #sidebar. Other mimicking of blog features.

--- post-preview.php
+++ post-preview.php
@@ -12,13 +12,13 @@ $title_parts = preg_split("/: /", $title_lines[0]);
    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 <head>
-   <title>Preview - <?php echo $title_parts[1] ?></title>
-	 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-   <link rel="stylesheet" type="text/css" media="all" href="style.css" />
-   <script type="text/javascript" src="<?php echo "$home/Library/JavaScript/MathJax/MathJax.js" ?>"></script>
-       <script type="text/javascript" src="styleLineNumbers.js"></script>
-        <script type="text/javascript" src="jquery-1.4.2.min.js"></script>
-        <script type="text/javascript" src="footnote-popup.js"></script>
+  <title>Preview - <?php echo $title_parts[1] ?></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <link rel="stylesheet" type="text/css" media="all" href="style.css" />
+  <script type="text/javascript" src="<?php echo "$home/Library/JavaScript/MathJax/MathJax.js" ?>"></script>
+  <script type="text/javascript" src="styleLineNumbers.js"></script>
+  <script type="text/javascript" src="jquery-1.4.2.min.js"></script>
+  <script type="text/javascript" src="footnote-popup.js"></script>
 </head>
 <body onload="styleLN()">
    <div id="container">
@@ -26,14 +26,18 @@ $title_parts = preg_split("/: /", $title_lines[0]);
          <h1>And now it's all this</h1>
          <h2>I just said what I said and it was wrong. Or was taken wrong.</h2>
       </div> <!-- header -->
-      <div id="sidebar">
-      </div> <!-- sidebar -->
       
       <div id="content">
         <h1><?php echo $title_parts[1] ?></h1>
         <?php echo SmartyPants(Markdown($parts[1])) ?>
-      </div> <!-- note -->
+      </div> <!-- content -->
+      
+      <div id="sidebar">
+       </div> <!-- sidebar -->
+      <div id="footer">
+      </div> <!-- footer -->
     </div> <!-- container -->
+    
   </body>
 </html>
 
