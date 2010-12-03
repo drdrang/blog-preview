7ae9c7232289f4e8defc59d63333288462e4ea1d
Dr. Drang <drdrang@gmail.com>
2010-12-03 00:25:39 -0600

    Changed paths to PHPMEM and PHP-SmartyPants.

--- post-preview.php
+++ post-preview.php
@@ -1,7 +1,7 @@
 <?php
 $home = getenv("HOME");
-require_once("$home/php-markdown-extra-math/markdown.php");
-require_once("$home/php-smartypants/smartypants.php");
+require_once("$home/git/php-markdown-extra-math/markdown.php");
+require_once("$home/git/php-smartypants/smartypants.php");
 $full = stream_get_contents(STDIN);
 $parts = preg_split("/\n\n/", $full, 2);
 $header_lines = preg_split("/\n/", $parts[0]);
