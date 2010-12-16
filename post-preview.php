ebdff64c94054825dd9c719b83246285454e969b
Dr. Drang <drdrang@gmail.com>
2010-12-16 08:50:56 -0600

    Change name of syntax highlighting CSS file.

--- post-preview.php
+++ post-preview.php
@@ -16,7 +16,6 @@ $title_parts = preg_split("/: /", $title_lines[0]);
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <link rel="stylesheet" type="text/css" media="all" href="style.css" />
   <link rel="stylesheet" type="text/css" media="all" href="syntax.css" />
-  <link rel="stylesheet" type="text/css" media="all" href="syntax-default.css" />
   <script type="text/javascript" src="<?php echo "$home/Library/JavaScript/MathJax/MathJax.js" ?>"></script>
   <script type="text/javascript" src="jquery-1.4.2.min.js"></script>
   <script type="text/javascript" src="highlight.pack.js"></script>
