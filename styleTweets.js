8881d8afe9dc11cfc1381396a4075a63fa06012f
Dr. Drang <drdrang@gmail.com>
2012-01-30 17:04:07 -0600

    Change link color to user's preference.

--- styleTweets.js
+++ styleTweets.js
@@ -11,14 +11,19 @@ function styleTweets() {
     $.getJSON(tweetURL, function(data){
       var twDate = new Date(data.created_at);
       var shortdate = twDate.toDateString();
+      // Remove plain attribution.
+      $("#" + divID + " .twMeta").css('display', 'none');
+      // Add styled attribution.
+      content = $("#" + divID + " .twContent").append('<p class="twDate"><a href="http://twitter.com/' + data.user.screen_name + '/status/' + tweetID + '">' + shortdate + '</a></p><p class="twAuthor"><a href="http://twitter.com/' + data.user.screen_name + '"><img src="' + data.user.profile_image_url + '" /></a><a href="http://twitter.com/' + data.user.screen_name + '"><strong>@' + data.user.screen_name + '</strong></a><br /><span class="realName">' + data.user.name + '</span></span></p>' );
+      // Set background image or color.
       if (data.user.profile_use_background_image == true) {
         $("#" + divID).css('background', 'url(' + data.user.profile_background_image_url + ') #' + data.user.profile_background_color);
       }
       else {
         $("#" + divID).css('background', '#' + data.user.profile_background_color);
       }
-      $("#" + divID + " .twMeta").css('display', 'none');
-      content = $("#" + divID + " .twContent").append('<p class="twDate"><a href="http://twitter.com/' + data.user.screen_name + '/status/' + tweetID + '">' + shortdate + '</a></p><p class="twAuthor"><a href="http://twitter.com/' + data.user.screen_name + '"><img src="' + data.user.profile_image_url + '" /></a><a href="http://twitter.com/' + data.user.screen_name + '"><strong>@' + data.user.screen_name + '</strong></a><br /><span class="realName">' + data.user.name + '</span></span></p>' );
+      // Set link color.
+      $("#" + divID + " a").css('color', '#' + data.user.profile_link_color);      
       });
   });
 }
