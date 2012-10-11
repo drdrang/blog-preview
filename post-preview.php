5e876eca5fe4b2755636fd647ef7810f02b1d5b5
Dr. Drang <drdrang@gmail.com>
2012-10-11 17:23:11 -0500

    Change source of highlight.js, don't style tweets dynamically.

--- post-preview.php
+++ post-preview.php
@@ -18,10 +18,10 @@ $title_parts = preg_split("/: /", $title_lines[0]);
   <link rel="stylesheet" type="text/css" media="all" href="syntax.css" />
   <script type="text/javascript" src="<?php echo "$home/Library/JavaScript/MathJax/MathJax.js" ?>"></script>
   <script type="text/javascript" src="jquery-1.4.2.min.js"></script>
-  <script type="text/javascript" src="highlight.pack.js"></script>
+  <script type="text/javascript" src="../highlight.js/build/highlight.pack.js"></script>
   <script type="text/javascript" src="styleCode.js"></script>
   <script type="text/javascript" src="footnote-popup.js"></script>
-  <script type="text/javascript" src="styleTweets.js"></script>
+  <!-- <script type="text/javascript" src="styleTweets.js"></script> -->
 </head>
 <body onLoad="styleCode()">
    <div id="container">
